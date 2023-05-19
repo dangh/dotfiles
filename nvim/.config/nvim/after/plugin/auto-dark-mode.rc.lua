8582016ff260b156273905ea77c311d1321c2df4
Dang <huynhminhdang@gmail.com>
2023-05-19 20:02:24 +0800

    nvim: auto dark

--- nvim/.config/nvim/after/plugin/auto-dark-mode.rc.lua
+++ nvim/.config/nvim/after/plugin/auto-dark-mode.rc.lua
@@ -5,11 +5,19 @@ auto_dark_mode.setup {
   update_interval = 1000,
   set_dark_mode = function()
     vim.api.nvim_set_option('background', 'dark')
-    -- vim.cmd('colorscheme gruvbox')
+    vim.cmd('let g:everforest_background = "hard"')
+    vim.cmd('let g:everforest_better_performance = 1')
+    vim.cmd('colorscheme everforest')
   end,
   set_light_mode = function()
-    vim.api.nvim_set_option('background', 'light')
-    -- vim.cmd('colorscheme gruvbox')
+    local background = 'light'
+    if (os.getenv('TMUX')) then
+      background = 'dark'
+    end
+    vim.api.nvim_set_option('background', background)
+    vim.cmd('let g:everforest_background = "hard"')
+    vim.cmd('let g:everforest_better_performance = 1')
+    vim.cmd('colorscheme everforest')
   end,
 }
 
